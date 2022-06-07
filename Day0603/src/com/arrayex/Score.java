39f767b0432f38293a4935fb0272a74a47caeb39
MaSHwan <tmdg3801@gmail.com>
2022-06-07 15:10:47 +0900

    Day0607

---
+++ Day0603/src/com/arrayex/Score.java
@@ -0,0 +1,10 @@
+package com.arrayex;
+
+public interface Score {
+	public void insert();	// 추가
+	public void update();	// 수정
+	public void delete();	// 삭제
+	public void listAll();
+	public void searchName();
+	public void searchhak();
+}
