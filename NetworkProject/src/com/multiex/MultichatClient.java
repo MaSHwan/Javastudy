791cde553f4998b34e76a98cbe691c4659521aaa
MaSHwan <tmdg3801@gmail.com>
2022-07-11 10:15:58 +0900

    ChatProgram

--- NetworkProject/src/com/multiex/MultichatClient.java
+++ NetworkProject/src/com/multiex/MultichatClient.java
@@ -37,38 +37,95 @@ public class MultichatClient implements Runnable, ActionListener {
 
 	// 입력패널
 	private JPanel msgPanel;
-	
+
 	// 입력텍스트 필드
 	private JTextField msgInput;
-	
+
 	// 종료버튼
 	private JButton exitb;
-	
-	//메인 윈도우 창
+
+	// 메인 윈도우 창
 	private JFrame jframe;
-	
-	//채팅 내용을 출력하는창
+
+	// 채팅 내용을 출력하는창
 	private JTextArea msgOut;
-	
+
 	// 카드 레이아웃
 	private Container tab;
 	private CardLayout clayout;
 	private Thread thread;
-	
+
 	// 상태 플래그
 	boolean status;
-	
+
 	public MultichatClient(String ip) {
 		this.ip = ip;
-		
+
 		// 로그인 패널 구성
 		loginPanel = new JPanel();
-		
+
 		// 레이아웃 설정
 		loginPanel.setLayout(new BorderLayout());
 		idInput = new JTextField(15);
 		loginb = new JButton("로그인");
-		
+
+		// 대화명 라벨
+		label1 = new JLabel("대화명");
+
+		// 패널에 대한 레이아웃 설정
+		loginPanel.add(label1, BorderLayout.WEST);
+		loginPanel.add(idInput, BorderLayout.CENTER);
+		loginPanel.add(loginb, BorderLayout.EAST);
+
+		// 로그아웃 패널구성
+		logoutPanel = new JPanel();
+
+		// 레이아웃 설정
+		logoutPanel.setLayout(new BorderLayout());
+		label2 = new JLabel();
+		logoutb = new JButton("로그아웃");
+
+		logoutPanel.add(label2, BorderLayout.CENTER);
+		logoutPanel.add(logoutb, BorderLayout.EAST);
+
+		// 입력 패널 구성
+		msgPanel = new JPanel();
+		msgPanel.setLayout(new BorderLayout());
+		msgInput = new JTextField(30);
+
+		exitb = new JButton("종료");
+
+		msgPanel.add(msgInput, BorderLayout.CENTER);
+		msgPanel.add(exitb, BorderLayout.EAST);
+
+		// 로그인/로그아웃 패널 선택을 위한 카드 레이아웃
+		tab = new JPanel();
+		clayout = new CardLayout();
+		tab.setLayout(clayout);
+		tab.add(loginPanel, "login");
+		tab.add(logoutPanel);
+		tab.add(logoutPanel, "logout");
+
+		// 메인 프레임 구성
+		jframe = new JFrame("멀티 채팅 프로그램");
+		msgOut = new JTextArea("", 10, 30);
+		msgOut.setEditable(false);
+
+		// 수직 스크롤 바는 항상 나타내고, 수평 스크롤바는 필요시에만 나타나게 함
+		JScrollPane jsp = new JScrollPane(msgOut, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+		jframe.add(tab, BorderLayout.NORTH);
+		jframe.add(jsp, BorderLayout.CENTER);
+		jframe.add(msgPanel, BorderLayout.SOUTH);
+
+		clayout.show(tab, "login");
+
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.pack(); // 프레임크기 자동설정
+		jframe.setResizable(false);
+		jframe.setVisible(true);
+
 	}
 
 	@Override
@@ -81,4 +138,7 @@ public class MultichatClient implements Runnable, ActionListener {
 
 	}
 
+	public static void main(String[] args) {
+		new MultichatClient("192.168.0.26");
+	}
 }
